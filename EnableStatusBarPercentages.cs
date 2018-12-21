d402621ca5951987869f655336bc46dec32c623c
zeobviouslyfakeacc <zeobviouslyfakeacc@users.noreply.github.com>
2018-12-21 20:32:56 +0100

    Fix status label positions, add condition percentage

--- EnableStatusBarPercentages.cs
+++ EnableStatusBarPercentages.cs
@@ -3,17 +3,30 @@ using UnityEngine;
 
 [HarmonyPatch(typeof(Panel_FirstAid), "Start")]
 public class EnableStatusBarPercentages {
+
+	private static readonly Vector3 statusLabelOffset = new Vector3(-27, -6);
+	private static readonly Vector3 conditionLabelOffset = new Vector3(71, -10);
+
 	private static void Postfix(Panel_FirstAid __instance) {
-		ActivateAndMove(__instance.m_ColdPercentLabel);
-		ActivateAndMove(__instance.m_FatiguePercentLabel);
-		ActivateAndMove(__instance.m_ThirstPercentLabel);
-		ActivateAndMove(__instance.m_HungerPercentLabel);
+		ActivateAndMoveStatusLabel(__instance.m_ColdPercentLabel);
+		ActivateAndMoveStatusLabel(__instance.m_FatiguePercentLabel);
+		ActivateAndMoveStatusLabel(__instance.m_ThirstPercentLabel);
+		ActivateAndMoveStatusLabel(__instance.m_HungerPercentLabel);
+		ActivateAndMoveConditionLabel(__instance.m_LabelConditionPercent);
 	}
 
-	private static void ActivateAndMove(UILabel label) {
+	private static void ActivateAndMoveStatusLabel(UILabel label) {
 		label.pivot = UIWidget.Pivot.Center;
-		label.gameObject.transform.localPosition = new Vector3(-79, -16, 0);
+		label.gameObject.transform.localPosition += statusLabelOffset;
 		label.fontSize = 14;
+
+		label.enabled = true;
+		NGUITools.SetActive(label.gameObject, true);
+	}
+
+	private static void ActivateAndMoveConditionLabel(UILabel label) {
+		label.pivot = UIWidget.Pivot.Left;
+		label.gameObject.transform.localPosition += conditionLabelOffset;
 
 		label.enabled = true;
 		NGUITools.SetActive(label.gameObject, true);
