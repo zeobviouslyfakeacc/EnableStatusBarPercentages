e5fdb8fd3cf2e57720a95ae49e9e803a9b601767
zeobviouslyfakeacc <zeobviouslyfakeacc@users.noreply.github.com>
2019-06-02 00:51:27 +0200

    Fix the position of the percentage labels

--- Patches.cs
+++ Patches.cs
@@ -13,20 +13,32 @@ internal static class Patches {
 	[HarmonyPatch(typeof(Panel_FirstAid), "Start")]
 	private static class EnableStatusBarPercentages {
 
-		private static readonly Vector3 statusLabelOffset = new Vector3(-27, -6);
+		private static readonly Vector3 containerOffset = new Vector3(10, 5);
+		private static readonly Vector3 statusLabelOffset = new Vector3(-72, -8);
 		private static readonly Vector3 conditionLabelOffset = new Vector3(72, -10);
 
 		private static void Postfix(Panel_FirstAid __instance) {
-			ActivateAndMoveStatusLabel(__instance.m_ColdPercentLabel);
-			ActivateAndMoveStatusLabel(__instance.m_FatiguePercentLabel);
-			ActivateAndMoveStatusLabel(__instance.m_ThirstPercentLabel);
-			ActivateAndMoveStatusLabel(__instance.m_HungerPercentLabel);
+			CenterStatusLabel(__instance.m_ColdStatusLabel);
+			CenterStatusLabel(__instance.m_FatigueStatusLabel);
+			CenterStatusLabel(__instance.m_ThirstStatusLabel);
+			CenterStatusLabel(__instance.m_HungerStatusLabel);
+
+			ActivateAndMovePercentLabel(__instance.m_ColdPercentLabel);
+			ActivateAndMovePercentLabel(__instance.m_FatiguePercentLabel);
+			ActivateAndMovePercentLabel(__instance.m_ThirstPercentLabel);
+			ActivateAndMovePercentLabel(__instance.m_HungerPercentLabel);
+
 			ActivateAndMoveConditionLabel(__instance.m_LabelConditionPercent);
 		}
 
-		private static void ActivateAndMoveStatusLabel(UILabel label) {
-			label.pivot = UIWidget.Pivot.Center;
-			label.gameObject.transform.localPosition += statusLabelOffset;
+		private static void CenterStatusLabel(UILabel label) {
+			label.alignment = NGUIText.Alignment.Center;
+			label.transform.localPosition += new Vector3(-label.width / 2, 0) + containerOffset;
+		}
+
+		private static void ActivateAndMovePercentLabel(UILabel label) {
+			label.alignment = NGUIText.Alignment.Left;
+			label.transform.localPosition += statusLabelOffset + containerOffset;
 			label.fontSize = 14;
 
 			label.enabled = true;
